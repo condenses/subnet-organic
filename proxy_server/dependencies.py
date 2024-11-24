14e291360dbea4c7c407ab92270ddb06c72420dc
toilaluan <tranthanhluan.nd@gmail.com>
2024-11-24 09:06:22 +0000

    update

--- proxy_server/dependencies.py
+++ proxy_server/dependencies.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI, Request
 from fastapi.exceptions import HTTPException
 import bittensor as bt
+import time
 
 
 def check_authentication(request: Request):
     message = request.headers.get("message")
     ss58_address = request.headers.get("ss58_address")
     signature = request.headers.get("signature")
+    authentication_key, nonce = message.split(":")
+    latency = time.time_ns() - int(nonce)
+    print(f"Latency: {latency}")
+    if latency / 1e9 > 48:
+        raise HTTPException(
+            status_code=401, detail=f"Invalid nonce, too old: {latency / 1e9}"
+        )
     keypair = bt.Keypair(ss58_address=ss58_address)
     print(f"Checking authentication for {ss58_address}")
     if not keypair.verify(message, signature):
@@ -14,4 +22,4 @@ def check_authentication(request: Request):
     print(f"Authenticated {ss58_address}")
     ip_address = request.headers.get("X-Real-Ip") or request.client.host
 
-    return ss58_address, ip_address, message
+    return ss58_address, ip_address, authentication_key
