df5021bcdeb74c1509e87103a0d56ec6e1aa1f13
Tung Nguyen <dinhtungtp0601@gmail.com>
2025-02-18 17:03:44 +0000

    force metagraph update by consecutive retrying

--- proxy_server/utils.py
+++ proxy_server/utils.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import random
 
 logger = logging.getLogger("Utils")
 
@@ -13,8 +14,23 @@ async def resync_in_background(metagraph):
     while True:
         try:
             logger.info("Resyncing subtensor metagraph...")
-            await asyncio.to_thread(metagraph.sync)  # Run the sync operation in a thread
+            sync_success = False
+            retry_count = 0
+            max_retries = 100
+
+            while not sync_success and retry_count < max_retries:
+                try:
+                    await asyncio.to_thread(metagraph.sync)  # Run the sync operation in a thread
+                    sync_success = True
+                except Exception as e:
+                    retry_count += 1
+                    if retry_count == max_retries:
+                        logger.error(f"Failed to sync metagraph after {max_retries} attempts: {e}")
+                        raise  # Re-raise the exception to be caught outside the loop
+                    sleep_time = random.uniform(3, 5)
+                    await asyncio.sleep(sleep_time)
+
             logger.info("Metagraph resync completed.")
         except Exception as e:
             logger.error(f"Error during metagraph resync: {e}")
-        await asyncio.sleep(600)  # Wait for 60 seconds before the next sync
+        await asyncio.sleep(600)  # Wait for 600 seconds before the next sync
